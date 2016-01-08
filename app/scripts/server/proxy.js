61005642bd7ea854ef77e09f8bba438d925477b5
krishna <krishnaof1988@gmail.com>
2016-01-08 17:36:07 +0530

    Redirection of www `a` card

--- app/scripts/server/proxy.js
+++ app/scripts/server/proxy.js
@@ -5,11 +5,16 @@ var log = require('npmlog');
 var proxy = httpProxy.createProxyServer({});
 
 http.createServer(function (req, res) {
-  console.log('URL :: ' + req.url);
+  log.info('URL :: ' + req.url);
   var urlServe = url.parse(req.url);
   if (urlServe.host.indexOf('.safenet') > -1) { // Temp fix - use regex
     if (urlServe.host.indexOf('api.safenet') !== 0) {
       var tokens = urlServe.host.split('.');
+      if (tokens.length === 3 && tokens[0] === 'www') { // redirect www.host to host
+        res.statusCode = 302;
+        res.setHeader('location', 'http://' + tokens[1] + '.' + tokens[2]);
+        return res.end();
+      }
       var service = tokens.length === 3 ? tokens[0] : 'www';
       var domain = tokens.length === 3 ? tokens[1] : tokens[0];
       var path = urlServe.pathname.split('/').slice(1).join('/') || 'index.html';
